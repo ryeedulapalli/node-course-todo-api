f910d3fb5f4c03acba913d2a504a421a77595adc
ryeedulapalli <raghuyeedullapalli@gmail.com>
2018-02-18 10:20:23 -0600

    Playground for user query

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,42 @@
+const {ObjectID} = require('mongodb');
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+var id = '5a89a08eb141c88c522b87dc11';
+
+if(!ObjectID.isValid(id)) {
+    console.log('ID not valid');
+}
+    
+Todo.find({
+    _id: id
+}).then((todos) => {
+    console.log('Todos', todos)
+});
+
+Todo.findOne({
+    _id: id
+}).then((todo) => {
+    console.log('Todo', todo)
+});
+
+Todo.findById(id).then((todo) => {
+    if(!todo) {
+        return console.log('id not found');
+    }
+    console.log('Todo by Id', todo)
+}).catch((e) => {
+    console.log(e);
+});
+
+var id = '5a88d41ce12d0de5462f7c8e';
+
+User.findById(id).then((user) => {
+    if(!user) {
+        return console.log('user not found');
+    }
+    console.log('User by ID', user);
+}).catch((e) => {
+    console.log(e);
+});
