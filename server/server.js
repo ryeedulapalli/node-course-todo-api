1514d452229e0b5e15c8c90b10270ea65b78458d
ryeedulapalli <raghuyeedullapalli@gmail.com>
2018-02-20 22:42:05 -0600

    Add GET /users/me authentication middleware

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose')
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 var {ObjectID} = require('mongodb');
 
 var app = express();
@@ -104,6 +105,12 @@ app.post('/users', (req, res) => {
     }).catch((e) => {
         res.status(400).send(e);
     })
+});
+
+
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 app.listen(port, () => {
